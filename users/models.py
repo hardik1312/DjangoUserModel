63cfb670ddfc2793249dcb9b83643613b6aa3ab3
Hardik Khatri <hardikkhatri1312@gmail.com>
2022-06-10 16:24:53 +0530

    Added AbstractBaseUser, Required Fields, PermissionsMixin, and BaseUserManager

--- users/models.py
+++ users/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 # Create your models here.
-class NewUser():
+class NewUser(AbstractBaseUser, PermissionsMixin, BaseUserManager):
 
     email = models.EmailField(_('email address'), unique=True)
     user_name = models.CharField(max_length=150, unique=True)
@@ -13,3 +14,9 @@ class NewUser():
         'about'), max_length=500, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS: 'user_name'
+
+    def ___str___(self):
+        return self.user_name
